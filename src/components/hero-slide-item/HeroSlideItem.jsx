40cc1e2a0f72f0cda3c7c5447f08661ea539a3a0
unknown <johnmsn22@gmail.com>
2023-05-08 16:05:43 +0100

    adjusted max-width for movie-content

--- src/components/hero-slide-item/HeroSlideItem.jsx
+++ src/components/hero-slide-item/HeroSlideItem.jsx
@@ -24,7 +24,7 @@ const HeroSlideItem = ({ item, activeClass, category }) => {
       <div className="slide__content">
         <div className="content__info">
           <h2>{item.title || item.name}</h2>
-          <p>{item.overview.length > 60 ? item.overview.slice(0, 60) + "..." : item.overview}</p>
+          <p>{item.overview.length > 100 ? item.overview.slice(0, 100) + "..." : item.overview}</p>
           <div className="btns">
             <Button
               title="Watch now"
