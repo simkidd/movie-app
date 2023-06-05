0f4dfbfa6d22720555806311a9c17c34150cb520
unknown <johnmsn22@gmail.com>
2023-06-05 08:58:05 +0100

    auth fixed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -62,9 +62,10 @@ const Header = () => {
     };
   }, []);
 
-  const avatarImage = currentUser
-    ? currentUser.photoUrl
-    : "https://res.cloudinary.com/demo/image/upload/w_100,h_100,c_thumb,g_face,r_20,d_avatar.png/non_existing_id.png";
+  const avatarImage =
+    currentUser && currentUser.photoURL
+      ? currentUser.photoURL
+      : "https://res.cloudinary.com/demo/image/upload/w_100,h_100,c_thumb,g_face,r_20,d_avatar.png/non_existing_id.png";
 
   return (
     <div className={`header ${isScrolled ? "scrolled" : ""}`}>
